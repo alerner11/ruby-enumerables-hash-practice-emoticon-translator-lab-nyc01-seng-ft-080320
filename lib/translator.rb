e23f8a85f3b61ba6d757108bd74045466e46027c
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-20 19:57:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,7 @@ def get_japanese_emoticon(path, emoticon)
       return organized_library[name][:japanese]
     end
   end
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(path, emoticon)
